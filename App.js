130267baf5ec7c4b78d33b6252898b63615cbdd5
Vitoria Belshoff <103432976+vitoriabelshoff@users.noreply.github.com>
2023-04-30 17:31:00 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,37 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
 import { useEffect, useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, Image} from 'react-native';
+
+import styles from './src/components/styles'
+
 
 export default function App() {
 
   let [filmes, setFilmes] = useState([]);
 
   const baseURL = 'https://api.otaviolube.com/api/filmes?populate=*';
+  const imageURL = 'https://api.otaviolube.com';
 
   useEffect(function(){
     fetch(baseURL)
-      .then(data => data.json())
-      .then(objeto => {
-        console.log(objeto);
-        setFilmes(objeto.data);
-      })
+    .then(data => data.json())
+    .then(objeto => {
+      console.log(objeto);
+      setFilmes(objeto.data);
+    })
   }, []);
 
   return (
-    <View style={styles.container}>
-      {filmes.length > 0 ? filmes.map(filme => <Text>
-        {filme.attributes.titulo}</Text>) :
-        <Text>Carregando...</Text>}
-      <StatusBar style="auto" />
-    </View>
+  <View style={styles.container}>
+    {(
+      filmes.map((filme) => (
+        <View key={filme._id} style={styles.content}>
+          <View style={styles.cardMovies}>
+            <Text style={styles.titulo}>{filme.attributes.titulo}</Text>
+            <Text style={styles.subtitulo}>{filme.attributes.subtitulo}</Text>  
+              <View style={styles.Img}>
+                  <Image style={styles.poster} source={{uri: imageURL+filme.attributes.poster.data.attributes.formats.small.url}} ></Image>
+              </View> 
+            <Text style={styles.tituloSinopse}>Sinopse</Text>
+            <Text style={styles.sinopse}>{filme.attributes.sinopse}</Text>
+          </View>
+        </View>
+      ))
+    )}
+    <StatusBar style="auto" />
+  </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
